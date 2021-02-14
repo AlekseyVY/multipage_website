ffb7c8bdfec13559727b923754357828c7136fba
alekseyvy <alekseyvy@gmail.com>
2021-02-14 17:16:10 +0400

    feature: added global typography, normilize.css global colors

---
+++ styles/colors.js
@@ -0,0 +1,10 @@
+
+// Primary colors
+export const peach = '#E7816B';
+export const black = '#1D1C1E';
+export const white = '#FFF';
+
+// Secondary Colors
+export const lightPeach = '#FFAD9B';
+export const darkGrey = '#333136';
+export const lightGrey = '#F1F3F5'
