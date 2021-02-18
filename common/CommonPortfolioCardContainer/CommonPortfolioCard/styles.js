ab16419455bbd2a438081557f4413e0599b4c025
alekseyvy <alekseyvy@gmail.com>
2021-02-18 17:59:48 +0400

    refactor: minor fix

--- common/CommonPortfolioCardContainer/CommonPortfolioCard/styles.js
+++ common/CommonPortfolioCardContainer/CommonPortfolioCard/styles.js
@@ -12,12 +12,13 @@ export const PortfolioCardContainer = styled.div`
   justify-content: flex-end;
   border-radius: 1.5rem;
   will-change: transform;
+
   &:hover {
     cursor: pointer;
-    transform:  scale(1.02);
-    -webkit-box-shadow: 0px 0px 20px 2px rgba(0,0,0,0.5);
-    -moz-box-shadow: 0px 0px 20px 2px rgba(0,0,0,0.5);
-    box-shadow: 0px 0px 20px 2px rgba(0,0,0,0.5);
+    transform: scale(1.02);
+    -webkit-box-shadow: 0 0 20px 2px rgba(0, 0, 0, 0.5);
+    -moz-box-shadow: 0 0 20px 2px rgba(0, 0, 0, 0.5);
+    box-shadow: 0 0 20px 2px rgba(0, 0, 0, 0.5);
     transition: 0.3s ease-in-out;
   }
 `
